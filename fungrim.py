1a831a6f2bfe45e8e22b1197d33067cd1dc2b91c
fredrik <fredrik.johansson@gmail.com>
2020-03-28 16:04:32 +0100

    star

--- fungrim.py
+++ fungrim.py
@@ -860,7 +860,7 @@ frontpage.fp.write("""</ul></li>""")
 frontpage.fp.write("""<li>Elliptic integrals<ul>""")
 writetopic("Arithmetic-geometric mean")
 writetopic("Legendre elliptic integrals")
-writetopic("Carlson symmetric elliptic integrals")
+writetopic("Carlson symmetric elliptic integrals", highlight=True)
 frontpage.fp.write("""</ul></li>""")
 
 frontpage.fp.write("""<li>Tables of sums, products, integrals...<ul>""")
